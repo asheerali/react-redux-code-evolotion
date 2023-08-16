a16ca3c08b29cf628875bbd038e63a671f66a897
Ali Asheer (IFAG BE DEV I C MC) <Asheer.Ali@infineon.com>
2023-08-16 12:55:13 +0200

    item container

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import store from './components/redux/store';
 import HookCakeContainer from './components/HookCakeContainer';
 import IceCreamContainer from './components/iceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/itemContainer';
 
 function App() {
   return (
     <Provider store={store}>  
     <div className="App">     
+    <ItemContainer cake />
+    <ItemContainer />
      <HookCakeContainer />
      <IceCreamContainer />
      <NewCakeContainer />
