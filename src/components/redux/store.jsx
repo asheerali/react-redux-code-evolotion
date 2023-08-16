7494b8efb451bf3b6aed2fe7ae01cb8ef3540949
Ali Asheer (IFAG BE DEV I C MC) <Asheer.Ali@infineon.com>
2023-08-16 12:13:06 +0200

    redux dev tools

--- src/components/redux/store.jsx
+++ src/components/redux/store.jsx
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer from "./cake/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
-// import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCream: iceCreamReducer,
   },
+  // middleware: [logger],
+  middleware: composeWithDevTools[logger],
 });
 
 export default store;
